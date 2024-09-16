f9a980a222ecefd091ac330636892ff0602023eb
python-sql09 <deepaponnusamy81@gmail.com>
2024-09-16 10:42:50 -0700

    python-practice2

--- myprojects/ETL-scripting/ectract-transform-load-static-method/transform.py
+++ myprojects/ETL-scripting/ectract-transform-load-static-method/transform.py
@@ -1,26 +1,27 @@
-#import sys
-#sys.path.append('/home/linuxdeepa/python-sql09/Python-SQL/myprojects/ETL-scripting/extract-transform-load')  # Add directory, not the file
-#from extract import extract
-# 11 The updated transform.py file
+# 3 Converting classes to static in the transform class
+from extract import extract
 class transform:
-    # Return the top N records from the dataset
-    def head(self, dataset, step):
+    #return the top N records from the dataset
+    @staticmethod
+    def head(dataset, step):
         if not dataset:
             raise Exception("Dataset cannot be empty.")
         if step < 1:
             raise Exception("The step value must be positive.")
-        return dataset[:step]
+        return dataset[0:step]
 
-    # Return the last N records from the dataset
-    def tail(self, dataset, step):
+    #return the last N records from the dataset
+    @staticmethod
+    def tail(dataset, step):
         if not dataset:
             raise Exception("Dataset cannot be empty.")
         if step < 1:
             raise Exception("The step value must be positive.")
-        return dataset[-step:]
+        return dataset[len(dataset) - step:len(dataset)]
 
     #rename a column in the dataset
-    def rename_attribute(self, dataset, attribute, new_attribute):
+    @staticmethod
+    def rename_attribute(dataset, attribute, new_attribute):
         if not dataset:
             raise Exception("Dataset cannot be empty.")
         if not attribute:
@@ -34,13 +35,14 @@ class transform:
                 new_row[new_attribute] = val
                 new_dataset.append(new_row)
             else:
-                raise Exception("Operation is not possible because the \
-column " + str(column_name) + " does not exist in one of the rows \
-in the dataset")
+                raise Exception("Operation is not possible because the column "
+                        + str(column_name)
+                        + " does not exist in one of the rows in the dataset.")
         return new_dataset
-    
+
     #remove a column from the dataset
-    def remove_attribute(self, dataset, attribute):
+    @staticmethod
+    def remove_attribute(dataset, attribute):
         new_dataset = list()
         for row in dataset:
             new_row = row
@@ -48,14 +50,14 @@ in the dataset")
                 del new_row[attribute]
                 new_dataset.append(new_row)
         return new_dataset
-    
-    #rename multiple columns from the dataset
-    def rename_attributes(self,dataset,attributes,new_attributes):
+
+    @staticmethod
+    def rename_attributes(dataset, attributes, new_attributes):
         if not attributes or not new_attributes:
             raise Exception("The attributes cannot be empty.")
         if len(attributes) != len(new_attributes):
             raise Exception("The number of new column names must match the number of \
-existing column names.")
+                existing column names.")
         new_dataset = list()
         for row in dataset:
             new_row = row
@@ -67,16 +69,17 @@ existing column names.")
                     del new_row[attribute]
                     new_row[new_attribute] = val
                 else:
-                    raise Exception("Operation is not possible because the key " + \
-str(key)+ \
-" does not exist in one of the rows in the dataset.")
+                    raise Exception("Operation is not possible because the key "
+                            + str(key)+
+                            " does not exist in one of the rows in the dataset.")
             new_dataset.append(new_row)
         return new_dataset
-    
-    #remove multiple column from the dataset
-    def remove_attributes(self, dataset, attributes):
+
+    #remove a column from the dataset
+    @staticmethod
+    def remove_attributes(dataset, attributes):
         if not dataset:
-            raise Exception("Dataset cannot be empty")
+            raise Exception("Dataset cannot be empty.")
         if not attributes:
             raise Exception("The list of attributes cannot be empty.")
         new_dataset= list()
@@ -87,9 +90,9 @@ str(key)+ \
                     del new_row[attribute]
             new_dataset.append(new_row)
         return new_dataset
-
-    # check the dataset and input is empty 
-    def transform(self, dataset, attribute, new_attribute, transform_function, *args):
+    
+    @staticmethod
+    def transform(dataset, attribute, new_attribute, transform_function, *args):
         if not dataset:
             raise Exception("Dataset cannot be empty.")
         if not attribute or not new_attribute:
@@ -98,8 +101,18 @@ str(key)+ \
             raise Exception("The transform_function cannot be None.")
         new_dataset = list() #output of this function
         for row in dataset: #iterate through the input data
-            t = transform_function(row[attribute], *args) #apply transformation function on column
+            t = transform_function(row[attribute], *args) #apply transformation
+                    #function on column
             z = row.copy()
             z.update({new_attribute:t}) #create new column in the data
             new_dataset.append(z)
         return new_dataset
+
+    def round_open_price(value,*args):
+        return round(float(value))
+    dataset = extract.fromCSV(file_path = "python-sql09/Python-SQL/mypractice-python/data/stocks.csv")
+    print(dataset[0])
+    new_dataset = transform.transform(dataset = dataset, attribute = "Open",
+                                      new_attribute = "open_price_rounded",
+                                      transform_function = round_open_price)
+    print(new_dataset[0])
